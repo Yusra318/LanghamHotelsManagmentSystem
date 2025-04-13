fa4b210a47e947cec9118d3837a620ec48eb603b
yusra <850005363@nzse.ac.nz>
2025-04-14 02:27:14 +1200

    change1: task 2 a

--- LanghamHotelsManagmentSystem/Program.cs
+++ LanghamHotelsManagmentSystem/Program.cs
@@ -8,102 +8,185 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+
 namespace Assessment2Task2
 {
     // Custom Class - Room
     public class Room
     {
-        public int RoomNo()
-    public bool IsAllocated()
+        public int RoomNumber { get; set; }
+        public bool IsAllocated { get; set; }
+
+        public Room(int number)
+        {
+            RoomNumber = number;
+            IsAllocated = false;
+        }
     }
+
     // Custom Class - Customer
     public class Customer
     {
-        public int CustomerNo()
-    public string CustomerName()
+        public int CustomerNumber { get; set; }
+        public string Name { get; set; }
+
+        public Customer(int number, string name)
+        {
+            CustomerNumber = number;
+            Name = name;
+        }
     }
+
     // Custom Class - RoomAllocation
-    public class RoomlAllocaltion
+    public class RoomAllocation
     {
-        public int AllocatedRoomNo()
-    public Customer AllocatedCustomer()
+        public int RoomNumber { get; set; }
+        public Customer AllocatedCustomer { get; set; }
+
+        public RoomAllocation(int roomNumber, Customer customer)
+        {
+            RoomNumber = roomNumber;
+            AllocatedCustomer = customer;
+        }
     }
+
     // Custom Main Class - Program
     class Program
     {
-        // Variables declaration and initialization
-        public static Room[] listofRooms;
-        public static int[] listOfRoomlAllocaltions;
-        public static string filePath;
-        // Main function
+        public static List<Room> listOfRooms = new List<Room>();
+        public static List<RoomAllocation> roomAllocations = new List<RoomAllocation>();
+
         static void Main(string[] args)
         {
-            string folderPath =
-            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            filePath = Path.Combine(folderPath, "HotelManagement.txt");
             char ans;
             do
             {
                 Console.Clear();
-                Console.WriteLine("****************************************************************
-                * ******************");
-                Console.WriteLine(" LANGHAM HOTEL MANAGEMENT SYSTEM
-                ");
-                Console.WriteLine(" MENU
-                ");
-                Console.WriteLine("****************************************************************
-                * ******************");
+                Console.WriteLine("****************************************************************");
+                Console.WriteLine("               LANGHAM HOTEL MANAGEMENT SYSTEM");
+                Console.WriteLine("                             MENU");
+                Console.WriteLine("****************************************************************");
                 Console.WriteLine("1. Add Rooms");
                 Console.WriteLine("2. Display Rooms");
                 Console.WriteLine("3. Allocate Rooms");
                 Console.WriteLine("4. De-Allocate Rooms");
                 Console.WriteLine("5. Display Room Allocation Details");
                 Console.WriteLine("6. Billing");
-                Console.WriteLine("7. Save the Room Allocations To a File");
-                Console.WriteLine("8. Show the Room Allocations From a File");
                 Console.WriteLine("9. Exit");
-                // Add new option 0 for Backup
-                Console.WriteLine("****************************************************************
-                * ******************");
-                Console.Write("Enter Your Choice Number Here:");
+                Console.WriteLine("****************************************************************");
+                Console.Write("Enter Your Choice Number Here: ");
                 int choice = Convert.ToInt32(Console.ReadLine());
+
                 switch (choice)
                 {
                     case 1:
-                        // adding Rooms function
+                        AddRooms();
                         break;
                     case 2:
-                        // display Rooms function;
+                        DisplayRooms();
                         break;
                     case 3:
-                        // allocate Room To Customer function
+                        AllocateRoom();
                         break;
                     case 4:
-                        // De-Allocate Room From Customer function
+                        DeallocateRoom();
                         break;
                     case 5:
-                        // display Room Alocations function;
+                        DisplayRoomAllocations();
                         break;
                     case 6:
-                        // Display "Billing Feature is Under Construction and will
-                        be added soon...!!!"
-                break;
-                    case 7:
-                        // SaveRoomAllocationsToFile
-                        break;
-                    case 8:
-                        //Show Room Allocations From File
+                        Console.WriteLine("Billing Feature is Under Construction and will be added soon...!!!");
                         break;
                     case 9:
-                        // Exit Application
+                        Console.WriteLine("Exiting... Thank you!");
                         break;
                     default:
+                        Console.WriteLine("Invalid choice.");
                         break;
                 }
-                Console.Write("\nWould You Like To Continue(Y/N):");
+
+                Console.Write("\nWould You Like To Continue(Y/N): ");
                 ans = Convert.ToChar(Console.ReadLine());
+
             } while (ans == 'y' || ans == 'Y');
+        }
+
+        static void AddRooms()
+        {
+            Console.Write("Enter number of rooms to add: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter Room Number: ");
+                int roomNo = Convert.ToInt32(Console.ReadLine());
+                listOfRooms.Add(new Room(roomNo));
+            }
+            Console.WriteLine("Room(s) Added Successfully!");
+        }
+
+        static void DisplayRooms()
+        {
+            Console.WriteLine("List of Rooms:");
+            foreach (Room room in listOfRooms)
+            {
+                Console.WriteLine($"Room No: {room.RoomNumber}, Allocated: {room.IsAllocated}");
+            }
+        }
+
+        static void AllocateRoom()
+        {
+            Console.Write("Enter Room Number to Allocate: ");
+            int roomNo = Convert.ToInt32(Console.ReadLine());
+
+            Room room = listOfRooms.Find(r => r.RoomNumber == roomNo);
+            if (room == null)
+            {
+                Console.WriteLine("Room not found.");
+                return;
+            }
+            if (room.IsAllocated)
+            {
+                Console.WriteLine("Room is already allocated.");
+                return;
+            }
+
+            Console.Write("Enter Customer Number: ");
+            int custNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Customer Name: ");
+            string custName = Console.ReadLine();
+
+            Customer customer = new Customer(custNo, custName);
+            roomAllocations.Add(new RoomAllocation(roomNo, customer));
+            room.IsAllocated = true;
+
+            Console.WriteLine("Room allocated successfully.");
+        }
+
+        static void DeallocateRoom()
+        {
+            Console.Write("Enter Room Number to Deallocate: ");
+            int roomNo = Convert.ToInt32(Console.ReadLine());
+
+            Room room = listOfRooms.Find(r => r.RoomNumber == roomNo);
+            if (room != null && room.IsAllocated)
+            {
+                roomAllocations.RemoveAll(ra => ra.RoomNumber == roomNo);
+                room.IsAllocated = false;
+                Console.WriteLine("Room de-allocated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Room is not allocated or does not exist.");
+            }
+        }
+
+        static void DisplayRoomAllocations()
+        {
+            Console.WriteLine("Room Allocation Details:");
+            foreach (RoomAllocation ra in roomAllocations)
+            {
+                Console.WriteLine($"Room No: {ra.RoomNumber}, Customer No: {ra.AllocatedCustomer.CustomerNumber}, Name: {ra.AllocatedCustomer.Name}");
+            }
         }
     }
 }
